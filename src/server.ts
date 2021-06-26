6ef9db6a770a8b4f3bfe2505be69a57bbee0f2a4
Rafhael Prates <rafhael@rafhaprates.dev>
2021-06-26 13:13:38 -0300

    chore: add .gitignore and add  sqllite in project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
+import "reflect-metadata";
 import express from "express";
 
 // @types/express
 const app = express();
+/**
+ * GET => Buscar uma informação 
+ * POST => Inserir (Criar) uma informação 
+ * PUT  => Alterar uma informcao 
+ * DELETE => Remover um dado
+ * PATCH => Alterar uma informacao especially
+ */
+
+/*******************************
+ * Tipos de parametros
+ * Routes Params  => Localhost:3000/produtos/13
+ * Query Params   => localhost:3000/produtos?name=teclado&description=tecladobom
+ * Body Params    => {
+ *  "name": "Produto1",
+ *  "description": "telado bom"
+ * }
+ */
+
+
 
 app.listen(3000, ()=> console.log("Servidor Iniciado") );
